1a1ebbd221d2305ecc13532a9d6fbeb68404f76c
Vincent Huss <vincent.huss@gmail.com>
2018-09-25 15:28:07 +0200

    vscode config

---
+++ ide-config/VSCODE/User/FileTemplates/model.interface.ts
@@ -0,0 +1,13 @@
+export interface #{filename}{
+	
+	
+};
+
+
+export function createMock(data:any = null):#{filename}{
+	let output:#{filename} = {
+		
+	};
+	output = {...output, ...data};
+	return output;
+};
